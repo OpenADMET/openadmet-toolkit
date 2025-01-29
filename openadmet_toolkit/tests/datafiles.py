643601f11c46b621c7a099079c4396a2ec9fc970
hmacdope <hugomacdermott@gmail.com>
2025-01-30 09:39:05 +1100

    improve test data further

--- openadmet_toolkit/tests/datafiles.py
+++ openadmet_toolkit/tests/datafiles.py
@@ -5,4 +5,4 @@ import openadmet_toolkit.tests.test_data  # noqa: F401
 _data_ref = resources.files("openadmet_toolkit.tests.test_data") # noqa: F841
 
 
-# example_file = (_data_ref / "example_file.txt").as_posix()
+example_file = (_data_ref / "example_file.txt").as_posix()
