4af5742daf538a86c63c7e367640fe32a85b90e8
cynthiaxu04 <cynthiaxu04@gmail.com>
2025-05-30 11:00:56 -0400

    created semiquant class

--- openadmet/toolkit/database/chembl.py
+++ openadmet/toolkit/database/chembl.py
@@ -545,3 +545,191 @@ class PermissiveChEMBLTargetCurator(ChEMBLTargetCuratorBase):
         group by (variant_id, tid, description, target_chembl_id, assay_id, assay_chembl_id) \
         """
         return self._chembl_connector.query(query, return_as=return_as)
+
+class SemiQuantChEMBLTargetCurator(ChEMBLTargetCuratorBase):
+    """
+    Implement all ChEMBL curation for a given protein target.
+
+    This curation includes both quantitative and semiquantitative data, i.e. activity data that is out of range of the assay.
+    In terms of ChEMBL curation, this means that 'standard_type' can be '=', '<', '>', '<=', or '>='.
+    pChEMBL value is explicitly NOT required.
+
+    """
+
+    standard_type: Optional[str] = Field(
+        None,
+        description="Select a single Standard type of the ChEMBL data (IC50, Ki, Kd, EC50, Potency).",
+    )
+    extra_filter: Optional[str] = Field(
+        None,
+        description="Extra filters to apply to the query, single word OR matching against assay description.",
+    )
+
+    @field_validator("standard_type")
+    def check_in_allowed_standard_types(cls, value):
+        allowed_standard_types = ["IC50", "Ki", "Kd", "EC50", "AC50", "Potency"]
+        if value not in allowed_standard_types:
+            raise ValueError(
+                f"Invalid standard type: {value}. Allowed values: {allowed_standard_types}"
+            )
+        return value
+
+    def get_activity_data(
+        self, return_as: str = "df"
+    ) -> Union[pd.DataFrame, duckdb.DuckDBPyRelation]:
+        """
+        Get all the activity data for a given target using its ChEMBL ID.
+        """
+        query = f"""
+        select
+        activities.assay_id                  as assay_id,
+        activities.doc_id                    as doc_id,
+        activities.standard_value            as standard_value,
+        activities.standard_relation         as standard_relation,
+        activities.standard_type             as standard_type,
+        activities.standard_units            as standard_units,
+        molecule_hierarchy.parent_molregno   as molregno,
+        compound_structures.canonical_smiles as canonical_smiles,
+        compound_structures.standard_inchi_key as standard_inchi_key,
+        target_dictionary.tid                as tid,
+        target_dictionary.chembl_id          as target_chembl_id,
+        pchembl_value                        as pchembl_value,
+        molecule_dictionary.pref_name        as compound_name,
+        activities.standard_type             as standard_type,
+        activities.bao_endpoint              as bao_endpoint,
+        assays.description                   as assay_description,
+        assays.assay_organism                as assay_organism,
+        assays.assay_strain                  as assay_strain,
+        assays.assay_tissue                  as assay_tissue,
+        assays.assay_type                    as assay_type,
+        assays.assay_cell_type               as assay_cell_type,
+        assays.assay_subcellular_fraction    as assay_subcellular_fraction,
+        assays.variant_id                    as variant_id,
+        assays.confidence_score              as confidence_score,
+        docs.year                            as doc_year,
+        docs.journal                         as doc_journal,
+        docs.doi                             as doc_doi,
+        docs.title                           as doc_title,
+        docs.authors                         as doc_authors,
+        docs.abstract                        as doc_abstract,
+        docs.patent_id                       as doc_patent_id,
+        docs.pubmed_id                       as doc_pubmed_id,
+        docs.chembl_release_id               as doc_chembl_release_id
+        from activities
+
+        join assays ON activities.assay_id = assays.assay_id
+        join target_dictionary ON assays.tid = target_dictionary.tid
+        join target_components ON target_dictionary.tid = target_components.tid
+        join component_class ON target_components.component_id = component_class.component_id
+        join molecule_dictionary ON activities.molregno = molecule_dictionary.molregno
+        join molecule_hierarchy ON molecule_dictionary.molregno = molecule_hierarchy.molregno
+        join compound_structures ON molecule_hierarchy.parent_molregno = compound_structures.molregno
+        join docs ON activities.doc_id = docs.doc_id
+        
+        where activities.standard_units = 'nM' and
+        activities.data_validity_comment IS null and
+        activities.potential_duplicate = 0 and
+        activities.standard_type IN ('IC50', 'XC50', 'EC50', 'AC50', 'Ki', 'Kd', 'Potency') and
+        target_chembl_id = '{self.chembl_target_id}'
+        """
+
+        all_data = self._chembl_connector.query(query, return_as="duckdb")
+
+        if return_as == "df":
+            return all_data.to_df()
+        else:
+            return all_data
+
+    def get_activity_data_for_compounds(
+        self, compounds: Iterable[str], canonicalise=False, detail=False
+    ) -> pd.DataFrame:
+        # convert list of smiles to INCHIKEY
+        if canonicalise:
+            with dm.without_rdkit_log():
+                inchikeys = [smiles_to_inchikey(canonical_smiles(x)) for x in compounds]
+        else:
+            inchikeys = [smiles_to_inchikey(x) for x in compounds]
+        # get all the activity data for the target
+        df = self.get_activity_data(return_as="df")
+        subset = df[df["standard_inchi_key"].isin(inchikeys)]
+        return subset
+
+    def aggregate_activity_data_by_compound(self, canonicalise=False) -> pd.DataFrame:
+        """
+        Get all the activity data for a given target using its ChEMBL ID, grouped by compound.
+        If canonicalise is True, the SMILES will be canonicalised and the InChIKey calculated and
+        aggregation will be done on the canonicalised SMILES and InChIKey.
+
+        Parameters
+        ----------
+        canonicalise: bool
+            Canonicalise the SMILES and calculate the InChIKey
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame containing the aggregated activity data.
+        """
+        all_data = self.get_activity_data(return_as="df")
+        if canonicalise:
+            with dm.without_rdkit_log():
+                all_data["OPENADMET_SMILES"] = all_data[
+                    "canonical_smiles"
+                ].progress_apply(lambda x: canonical_smiles(x))
+                all_data["OPENADMET_INCHIKEY"] = all_data[
+                    "OPENADMET_SMILES"
+                ].progress_apply(lambda x: smiles_to_inchikey(x))
+                data = all_data.groupby(["OPENADMET_SMILES", "OPENADMET_INCHIKEY"]).agg(
+                    {
+                        "assay_id": "count",
+                        "standard_value": ["mean", "median", "std"],
+                        "pchembl_value": ["mean", "median", "std"],
+                    }
+                )
+        else:
+            data = all_data.groupby(["molregno", "canonical_smiles"]).agg(
+                {
+                    "assay_id": "count",
+                    "standard_value": ["mean", "median", "std"],
+                    "pchembl_value": ["mean", "median", "std"],
+                    "compound_name": "first",  # or "unique" if you want all names
+                }
+            )
+        # unnenest column hierarchy
+        data.columns = ["_".join(col) for col in data.columns]
+        data = data.reset_index()
+        data.sort_values("assay_id_count", ascending=False, inplace=True)
+
+        return data
+
+    def get_variant_ids_for_target(
+        self, return_as: str = "df"
+    ) -> Union[pd.DataFrame, duckdb.DuckDBPyRelation]:
+        """
+        Get all the variant IDs for a given target using its ChEMBL ID
+        Only target_type = 'SINGLE PROTEIN' curation is applied.
+
+        Parameters
+        ----------
+        return_as: str
+            Return the data as a DataFrame or a DuckDB relation.
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame containing the variant IDs for the target
+
+        """
+        query = f"""
+        select distinct(variant_id), tid, description,  targets.chembl_id as target_chembl_id, \
+        assay_id, assays.chembl_id assay_chembl_id \
+        from activities \
+        join assays using(assay_id)  \
+        join docs on (assays.doc_id = docs.doc_id)  \
+        join target_dictionary as targets using (tid) \
+        where target_type = 'SINGLE PROTEIN' \
+        and target_chembl_id = '{self.chembl_target_id}' \
+        and variant_id is not null
+        group by (variant_id, tid, description, target_chembl_id, assay_id, assay_chembl_id) \
+        """
+        return self._chembl_connector.query(query, return_as=return_as)
