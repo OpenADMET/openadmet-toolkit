40f78290e6a3b9a1d8c8f23f726a71ac7e6d3c63
Cynthia <89044958+cynthiaxu04@users.noreply.github.com>
2025-08-07 15:44:56 -0400

    Add feature for curating datasets for multitask modeling (#78)
    * Add feature for curating datasets for multitask modeling
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update pyproject.toml to include new .parquet test files
    
    * Correct multi data curation test
    
    * Update MultiDataProcessing with corrections
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update openadmet/toolkit/chemoinformatics/data_curation.py
    
    Co-authored-by: Sean M. Colby <seanmcolby@gmail.com>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Sean M. Colby <seanmcolby@gmail.com>

--- openadmet/toolkit/tests/datafiles.py
+++ openadmet/toolkit/tests/datafiles.py
@@ -8,3 +8,9 @@ _data_ref = resources.files("openadmet.toolkit.tests.test_data")  # noqa: F841
 chembl_file = (_data_ref / "chembl_1A2_test_data.csv").as_posix()
 pubchem_file = (_data_ref / "AID_410_pubchem_test_data.csv").as_posix()
 filtering_file = (_data_ref / "filtering_test_data.csv").as_posix()
+
+# data yamls
+chembl_data_yaml = (_data_ref / "pchembl_data.yaml").as_posix()
+processed_chembl_data_yaml = (_data_ref / "processed_pchembl_data.yaml").as_posix()
+pubchem_data_yaml = (_data_ref / "pubchem_data.yaml").as_posix()
+processed_pubchem_data_yaml = (_data_ref / "processed_pubchem_data.yaml").as_posix()
